ab20ca4b5166cb0a72520fa73a4582b768c8c162
bo2013 <liuphuc06@gmail.com>
2026-08-07 20:26:52 +0700

    add isadmin check

--- server/src/routes/api/v1/songs/upload-file.ts
+++ server/src/routes/api/v1/songs/upload-file.ts
@@ -1,11 +1,12 @@
 import type { FastifyRequest, FastifyReply, RouteOptions } from "fastify"
 
 import { auth } from "../../../../middleware/auth.ts"
+import { isAdmin } from "../../../../middleware/isAdmin.ts"
 
 export default {
     method: "POST",
     url: "/api/v1/data/songs/file",
-    onRequest: auth,
+    onRequest: [auth, isAdmin],
     handler
 } satisfies RouteOptions
 
