9c648ad2c3c3162e97d99b84c9a9313ff07c5240
bo2013 <liuphuc06@gmail.com>
2026-08-07 20:26:28 +0700

    Name fix

--- server/src/middleware/isAdmin.ts
+++ server/src/middleware/isAdmin.ts
@@ -2,7 +2,7 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 
 import { GetUserByUUID } from "../services/user.ts";
 
-export async function auth(request: FastifyRequest, reply: FastifyReply) {
+export async function isAdmin(request: FastifyRequest, reply: FastifyReply) {
   const user = await GetUserByUUID(request.user.sub);
 
   if (user?.isAdmin !== true) {
