fe9007f3a043623ca9c6b358da9c47f66fda8909
Boo2013 <130146267+bo2013@users.noreply.github.com>
2026-08-16 13:12:38 +0700

    Implement songs all API response

--- server/src/routes/api/v1/songs/all.ts
+++ server/src/routes/api/v1/songs/all.ts
@@ -3,6 +3,15 @@ import type { FastifyRequest, FastifyReply, RouteOptions } from "fastify"
 import { auth } from "../../../../middleware/auth.ts"
 import { fb_songs } from "../../../../firebase.ts"
 
+type SongDoc = {
+    name: string
+    image: string
+    artists: {
+        id: string
+        name: string
+    }[]
+}
+
 export default {
     method: "GET",
     url: "/api/v1/songs/all",
@@ -11,9 +20,14 @@ export default {
 } satisfies RouteOptions
 
 async function handler(request: FastifyRequest, reply: FastifyReply) {
-    const snapshot = await fb_songs.get()
+    const snapshot = await fb_songs.get<SongDoc>()
 
-    const songs = snapshot.docs.map(doc => ({key: doc.id, ...doc.data()}))
+    const songs = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().name,
+        image: doc.data().image,
+        artists: doc.data().artists
+    }))
 
     return reply.send(songs)
 }
